abeeb430816dcc47d4c4abe5eb92d16b13e6173b
Jamie Thalman <jamie.thalman@gmail.com>
2013-12-18 09:46:12 -0800

    Merge branch 'master' of https://github.com/nice-work/nice-work.github.io
    Conflicts:
    _layouts/default.html

--- assets/js/application.js
+++ assets/js/application.js
@@ -5,6 +5,25 @@ $(function() {
 
   $('.menu-toggle').on('click', toggleMenu);
 
+  $('.open-buy-overlay').on('click', function() {
+    $('.buy-overlay').addClass('on');
+  });
+
+  $('.close-buy-overlay').on('click', function() {
+    $('.buy-overlay').removeClass('on');
+  });
+
+  $('.buy-overlay-change-image').on('click', function() {
+    var $currentImage = $('.buy-overlay-image'),
+      currentImageIndex;
+
+    currentImageIndex = parseInt($currentImage.attr('src').match(/.*\/(\d).jpg/)[1]) - 1;
+    currentImageIndex = $(this).hasClass('next') ? ++currentImageIndex : --currentImageIndex;
+    currentImageIndex = (currentImageIndex % 4) + 1;
+
+    $currentImage.attr('src', '/assets/images/issue-1/buy/' + currentImageIndex + '.jpg');
+  })
+
   function startVideo() {
     $('.video-overlay').addClass('on');
     $('.video-embed').attr('src', 'http://player.vimeo.com/video/70818122?autoplay=1');
